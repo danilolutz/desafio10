f8348012494b1d2fa261966eb6c8cc0dd989b510
Danilo Lutz <danilolutz@protonmail.com>
2020-08-20 22:05:16 -0300

    Finish challenge

---
+++ README.md
@@ -0,0 +1,45 @@
+# GoRestaurant Web
+
+[![MIT](https://img.shields.io/github/license/danilolutz/desafio10)](https://img.shields.io/github/license/danilolutz/desafio10)
+
+## :rocket: Overview
+
+Tenth challenge of Rocketseat GoStack.
+
+## :sunglasses: Get Started
+
+To run this project just download or clone it and then on root folder execute:
+
+```bash
+yarn
+```
+
+> **NOTE**: This will install all dependencies.
+
+### Running the project
+
+To run the project just execute:
+
+```bash
+ yarn json-server server.json -p 3333
+```
+
+```bash
+yarn dev:server
+```
+
+### Testing the project
+
+To test the project just execute:
+
+```bash
+yarn test
+```
+
+## :scroll: License
+
+This project is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+
+---
+
+Made with :purple_heart: by [Danilo Lutz](https://www.linkedin.com/in/danilolutz/)
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -27,7 +27,8 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadFoods(): Promise<void> {
-      // TODO LOAD FOODS
+      const response = await api.get('/foods');
+      setFoods(response.data);
     }
 
     loadFoods();
@@ -37,7 +38,8 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     try {
-      // TODO ADD A NEW FOOD PLATE TO THE API
+      const { data } = await api.post('/foods', food);
+      setFoods(oldFoods => [...oldFoods, data]);
     } catch (err) {
       console.log(err);
     }
@@ -46,11 +48,21 @@ const Dashboard: React.FC = () => {
   async function handleUpdateFood(
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
-    // TODO UPDATE A FOOD PLATE ON THE API
+    const { data } = await api.put(`/foods/${editingFood.id}`, {
+      ...food,
+      available: editingFood.available,
+    });
+
+    const updateFoods = foods.map(item => (item.id === data.id ? data : item));
+    setFoods(updateFoods);
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
-    // TODO DELETE A FOOD PLATE FROM THE API
+    await api.delete(`/foods/${id}`);
+
+    const cleannedFoods = foods.filter(food => food.id !== id);
+
+    setFoods(cleannedFoods);
   }
 
   function toggleModal(): void {
@@ -62,7 +74,8 @@ const Dashboard: React.FC = () => {
   }
 
   function handleEditFood(food: IFoodPlate): void {
-    // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food);
+    toggleEditModal();
   }
 
   return (
